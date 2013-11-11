ec95239e37a29b29dd932820c435399de2657272
Thorben Schröder <stillepost@gmail.com>
2013-11-11 20:58:22 +0100

    Adds XING links for speakers

--- schedule.rb
+++ schedule.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'open-uri'
 
 class Schedule
-  URL = 'https://docs.google.com/spreadsheet/pub?key=0Au_LuhlBQUkEdHRSc3Y2Q3h1SEstVXlGVmw2RG80U0E&single=true&gid=0&range=A6%3AD99&output=csv'
+  URL = 'https://docs.google.com/spreadsheet/pub?key=0Au_LuhlBQUkEdHRSc3Y2Q3h1SEstVXlGVmw2RG80U0E&single=true&gid=0&range=A6%3AE99&output=csv'
 
   def initialize
     @now  = Time.now
@@ -21,11 +21,13 @@ class Schedule
   def sessions
     day_of_event = Time.new(2013,11,22).at_midnight
 
-    last_topic, last_speaker = []
-
     sessions = @doc.map do |line|
-      time, topic, speaker = line
+      time, topic, speaker, clinic, link = line
       hours, minutes = time.split(':').map(&:to_i)
+
+      speakers = speaker ? speaker.split(' / ') : []
+      links = link && !link.empty? ? link.split('|') : []
+      links.map! {|l| l ? l.strip : nil}
 
       one_minute = 60
       one_hour = 60 * one_minute
@@ -33,7 +35,7 @@ class Schedule
       time = day_of_event + (hours * one_hour) + (minutes * one_minute)
 
       if topic && !topic.blank? && speaker && !speaker.blank?
-        {'info' => topic, 'speaker' => speaker, 'time' => time}
+        {'info' => topic, 'speakers' => speakers, 'time' => time, 'links' => links}
       else
         nil
       end
